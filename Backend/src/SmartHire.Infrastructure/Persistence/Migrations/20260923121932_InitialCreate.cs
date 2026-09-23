fb29ad28a7136ffde1c02150f79e8808173315a9
Phuc <trantrongphuc1501@gmail.com>
2026-09-23 19:24:42 +0700

    setup: check connection and create first migration

---
+++ Backend/src/SmartHire.Infrastructure/Persistence/Migrations/20260923121932_InitialCreate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartHire.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:vector", ",,");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
